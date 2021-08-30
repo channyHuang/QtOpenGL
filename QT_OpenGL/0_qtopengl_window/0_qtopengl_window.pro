884bdf9856854750b4bbffbdfebab40e44472c5a
channyHuang <349117102@qq.com>
2021-08-31 00:02:41 +0800

    add test in Mac

--- QT_OpenGL/0_qtopengl_window/0_qtopengl_window.pro
+++ QT_OpenGL/0_qtopengl_window/0_qtopengl_window.pro
@@ -15,7 +15,12 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+win32 {
 LIBS += -lOpengl32
+}
+macx {
+LIBS+= -framework opengl -framework glut
+}
 
 SOURCE_FILES = $$files($$PWD/*.cpp, false)
 HEADER_FILES = $$files($$PWD/*.h, false)
--- QT_OpenGL/0_qtopengl_window/main.cpp
+++ QT_OpenGL/0_qtopengl_window/main.cpp
@@ -6,14 +6,14 @@ int main(int argc, char *argv[])
 {
 	bool fs=false;
 	QApplication a(argc, argv);
-	switch(QMessageBox::information(0,"FullScreen?","Fullscreen or not?",QMessageBox::Yes,QMessageBox::No|QMessageBox::Default)) {
+    /*switch(QMessageBox::information(0,"FullScreen?","Fullscreen or not?",QMessageBox::Yes,QMessageBox::No|QMessageBox::Default)) {
 	case QMessageBox::Yes:
 		fs=true;
 		break;
 	case QMessageBox::No:
 		fs=false;
 		break;
-	}
+    }*/
 	nehewidget w(0,fs);
 	w.show();
 	return a.exec();
